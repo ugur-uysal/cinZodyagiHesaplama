77304c30aa71e66802062f20a1860ab72bdf39a9
ugur <uysallugur@outlook.com>
2023-12-22 18:48:00 +0300

    Java ile kullanıcıdan doğum tarihini alıp Çin Zodyağı değerini hesaplayan program yazınız.

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int dogumYili, kalan;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Doğum Yılınızı Giriniz : ");
+        dogumYili = input.nextInt();
+
+        kalan = dogumYili % 12;
+        switch (kalan) {
+            case 0:
+                System.out.print("Çin Zodyağı Burcunuz : Maymun");
+                break;
+            case 1:
+                System.out.print("Çin Zodyağı Burcunuz : Horoz");
+                break;
+            case 2:
+                System.out.print("Çin Zodyağı Burcunuz : Köpek");
+                break;
+            case 3:
+                System.out.print("Çin Zodyağı Burcunuz : Domuz");
+                break;
+            case 4:
+                System.out.print("Çin Zodyağı Burcunuz : Fare");
+                break;
+            case 5:
+                System.out.print("Çin Zodyağı Burcunuz : Öküz");
+                break;
+            case 6:
+                System.out.print("Çin Zodyağı Burcunuz : Kaplan");
+                break;
+            case 7:
+                System.out.print("Çin Zodyağı Burcunuz : Tavşan");
+                break;
+            case 8:
+                System.out.print("Çin Zodyağı Burcunuz : Ejderha");
+                break;
+            case 9:
+                System.out.print("Çin Zodyağı Burcunuz : Yılan");
+                break;
+            case 10:
+                System.out.print("Çin Zodyağı Burcunuz : At");
+                break;
+            case 11:
+                System.out.print("Çin Zodyağı Burcunuz : Koyun");
+                break;
+        }
+
+    }
+}
